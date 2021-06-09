f7e4699fe9e4ea3803629eb2498cd056e16026ab
PoetaKodu <rfpawelsyska@gmail.com>
2021-06-09 15:55:49 +0200

    Fixed critical bug (`reserve()` -> `resize()`)

--- src/Readers/General.cpp
+++ src/Readers/General.cpp
@@ -10,9 +10,10 @@ std::string readFileContents(fs::path const& path_)
 	if (!input.is_open())
 		throw std::runtime_error(fmt::format("failed to open file {} for reading", path_.string()));
 	
+
 	std::string result;
 	std::string buf;
-	buf.reserve(4 * 1024 * 1024);
+	buf.resize(4 * 1024 * 1024);
 	while(input.read(buf.data(), buf.capacity()))
 		result.append(buf.data(), input.gcount());
 	result.append(buf.data(), input.gcount());
